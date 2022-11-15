708e400e5718dcbe9df7acf475ceff548d885ce1
mustafa-bilen <mustafabilen6669@gmail.com>
2022-11-15 16:55:52 +0300

    storedone

---
+++ src/app/store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+import newsReducer from "../features/newsSlice";
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    news: newsReducer,
+  },
+});
